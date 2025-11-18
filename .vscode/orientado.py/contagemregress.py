4a7e693114a377baf4a17df482a21845267be482
Theo Carvalho Pereira <thcp211005@gmail.com>
2025-11-18 09:17:31 -0300

    chore: Remove arquivos de build e aplica .gitignorechore: Remove arquivos de build e aplica .gitignore

--- .vscode/orientado.py/contagemregress.py
+++
@@ -1,9 +0,0 @@
-import time
-
-print("Contagem regressiva")
-
-for contagem in range(10, 0, -1):
-    print(contagem)
-    time.sleep(2)
-
-print("Feliz ano novo!")
--- .vscode/orientado.py/piramide.py
+++
@@ -1,8 +0,0 @@
-rows = int(input("Entre com o número de rows"))
-
-for i in range(rows):
-    for j in range(rows - i - 1):
-        print(" ", end="")
-    for j in range(2 * i + 1):
-        print("*", end="")
-print()        
